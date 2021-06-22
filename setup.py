f3a02b51e7758a2d4d734aafe240b535cc51e1fd
lokijuhy <laura.kinkead@uzh.ch>
2021-06-22 11:56:22 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='code_sync',
-      version='0.1.0',
+      version='0.2.0',
       description='',
       url='https://github.com/uzh-dqbm-cmi/code-sync',
       packages=find_packages(),
